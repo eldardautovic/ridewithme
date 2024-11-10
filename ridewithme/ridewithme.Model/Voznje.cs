c69357d880305bd2e8e5ab76fd707ee2b5035ad2
Eldar Dautović <eldarcoox123@gmail.com>
2024-11-10 18:37:54 +0100

    Inicijalni commit.

---
+++ ridewithme/ridewithme.Model/Voznje.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ridewithme.Model
+{
+    public class Voznje
+    {
+        public int VoznjaId { get; set; }
+        public string Destinacija { get; set; }
+
+        public decimal Cijena { get; set; }
+
+    }
+}
---
+++ ridewithme/ridewithme.Service/VoznjeService.cs
@@ -0,0 +1,26 @@
+﻿using ridewithme.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ridewithme.Service
+{
+    public class VoznjeService : IVoznjeService
+    {
+        public List<Voznje> List = new List<Voznje>()
+        {
+            new Voznje()
+            {
+                VoznjaId = 1,
+                Cijena = 10,
+                Destinacija = "Elce"
+            }
+        };
+        public List<Voznje> GetList()
+        {
+            return List;
+        }
+    }
+}
