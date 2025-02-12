fd33a3a47d37653fdeeed0dd86474cb6fdef9cab
Eldar Dautović <eldarcoox123@gmail.com>
2025-02-12 14:49:47 +0100

    Inicijalizacija Mobilne aplikacije, kreiran onboarding ekran, kompletiran login ekran

---
+++ UI/ridewithme_mobile/lib/providers/korisnik_provider.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:ridewithme_mobile/models/korisnik.dart';
+import 'package:ridewithme_mobile/providers/base_provider.dart';
+
+class KorisnikProvider extends BaseProvider<Korisnik> {
+  KorisnikProvider() : super("Korisnici");
+
+  @override
+  fromJson(data) {
+    // TODO: implement fromJson
+    return Korisnik.fromJson(data);
+  }
+
+  Future<Korisnik> login(String username, String password) async {
+    var url = "$fullUrl/login?username=${username}&password=${password}";
+
+    print(url);
+
+    var uri = Uri.parse(url);
+    var headers = createHeaders();
+
+    var response = await http.post(uri, headers: headers);
+
+    if (isValidResponse(response)) {
+      var data = jsonDecode(response.body);
+
+      return fromJson(data);
+    } else {
+      throw new Exception("Unknown error");
+    }
+    // print("response: ${response.request} ${response.statusCode}, ${response.body}");
+  }
+}
---
+++ UI/ridewithme_mobile/lib/utils/auth_util.dart
@@ -0,0 +1,6 @@
+class Authorization {
+  static String? username;
+  static String? password;
+  static int? id;
+  static String? fullName;
+}
