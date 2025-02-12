9a9c304aa2b6335aa8bab55b05448609cfca6554
Eldar Dautović <eldarcoox123@gmail.com>
2025-02-12 15:05:27 +0100

    Modifikovan localhost uri, zbog razlike sa emulatorom.

--- UI/ridewithme_mobile/lib/providers/base_provider.dart
+++ UI/ridewithme_mobile/lib/providers/base_provider.dart
@@ -15,7 +15,7 @@ abstract class BaseProvider<T> with ChangeNotifier {
   BaseProvider(String endpoint) {
     _endpoint = endpoint;
     _baseUrl = const String.fromEnvironment("baseUrl",
-        defaultValue: "http://localhost:5284/api/");
+        defaultValue: "http://10.0.2.2:5284/api/");
   }
 
   Future<SearchResult<T>> get({dynamic filter}) async {
@@ -101,6 +101,7 @@ abstract class BaseProvider<T> with ChangeNotifier {
   }
 
   bool isValidResponse(Response response) {
+    print(response.body);
     if (response.statusCode < 299) {
       return true;
     } else if (response.statusCode == 401) {
