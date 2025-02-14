eeba73bbaadd733e0d7a0bee105ab4f14c75479e
Eldar Dautović <eldarcoox123@gmail.com>
2025-02-14 15:16:33 +0100

    Ispravljen padding na search widgetu

--- UI/ridewithme_mobile/lib/screens/voznje_screen.dart
+++ UI/ridewithme_mobile/lib/screens/voznje_screen.dart
@@ -73,7 +73,8 @@ class _VoznjeScreenState extends State<VoznjeScreen> {
             spacing: 15,
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 20, bottom: 10.0, top: 10),
+                padding: const EdgeInsets.only(
+                    left: 20, bottom: 10.0, top: 10, right: 20),
                 child: CupertinoSearchTextField(
                   placeholder: "Pretraži vožnje...",
                   onTap: () {
